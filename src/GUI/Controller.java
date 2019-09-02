1911a8300661b16114020ef83818ff3da43e1610
Jose Alejandro Chavarria <joalchama@gmail.com>
2019-09-02 15:53:35 -0600

    botones y controller

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,4 +1,45 @@
 package GUI;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
+/**
+ * Sample Skeleton for 'UI2.fxml' Controller Class
+ */
 public class Controller {
+
+        @FXML // fx:id="x1"
+        private Font x1; // Value injected by FXMLLoader
+
+        @FXML // fx:id="x2"
+        private Color x2; // Value injected by FXMLLoader
+
+        @FXML // fx:id="AND"
+        private ImageView AND; // Value injected by FXMLLoader
+
+        @FXML // fx:id="NAND"
+        private ImageView NAND; // Value injected by FXMLLoader
+
+        @FXML // fx:id="OR"
+        private ImageView OR; // Value injected by FXMLLoader
+
+        @FXML // fx:id="NOR"
+        private ImageView NOR; // Value injected by FXMLLoader
+
+        @FXML // fx:id="NOT"
+        private ImageView NOT; // Value injected by FXMLLoader
+
+        @FXML // fx:id="XOR"
+        private ImageView XOR; // Value injected by FXMLLoader
+
+        @FXML // fx:id="XNOR"
+        private ImageView XNOR; // Value injected by FXMLLoader
+
+        @FXML // fx:id="x3"
+        private Font x3; // Value injected by FXMLLoader
+
+        @FXML // fx:id="x4"
+        private Color x4; // Value injected by FXMLLoader
+
 }
--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("UI2.fxml"));
         primaryStage.setTitle("Hephaestus Circuit Designer");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
